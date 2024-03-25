b14db5e543046f27f9bb2dbce2f633f6c2fbb7cc
AnaCipolla <146762760+AnaCipolla@users.noreply.github.com>
2024-03-25 08:45:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,6 @@ for(let i=0;i <botoes.length;i++){
        texto[botoes[i].classList.add("ativo");
     }
 }
+
+const contadores =document.queryselectorAll(".contador.");
+const tempoObjetivo1 =new Date("2024-03-25T00:00:00");
